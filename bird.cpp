2ee7e2b2768d1e0b9e1465f8ec58f399330cadec
Andrew Turnbull <35672478+ajat69@users.noreply.github.com>
2024-10-25 10:39:19 -0400

    Minor tweek

--- bird.cpp
+++ bird.cpp
@@ -44,13 +44,15 @@
   * RANDOM
   * These functions generate a random number.
   ****************************************************************/
-int randomInt(int min, int max) {
+int randomInt(int min, int max) 
+{
 	assert(min < max);
 	int num = (rand() % (max - min)) + min;
 	assert(min <= num && num <= max);
 	return num;
 }
-double randomFloat(double min, double max) {
+double randomFloat(double min, double max) 
+{
 	assert(min <= max);
 	double num = min + ((double)rand() / (double)RAND_MAX * (max - min));
 	assert(min <= num && num <= max);
