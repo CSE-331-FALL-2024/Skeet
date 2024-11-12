3eb38e423ff4668f9e08754a4a732cc65dbd4ec8
Andrew Turnbull <35672478+ajat69@users.noreply.github.com>
2024-11-12 10:43:44 -0500

    Modified Storage Elements

--- storage_element.cpp
+++ storage_element.cpp
@@ -1,183 +1,183 @@
 #include "storage_element.h"
 #include <cassert>
 
-/******************************************************************
- * RANDOM
- * These functions generate a random number.
- ****************************************************************/
-int randomIntStorage(int min, int max)
-{
-   assert(min < max);
-   int num = (rand() % (max - min)) + min;
-   assert(min <= num && num <= max);
-   return num;
-}
-double randomFloatStorage(double min, double max)
-{
-   assert(min <= max);
-   double num = min + ((double)rand() / (double)RAND_MAX * (max - min));
-   assert(min <= num && num <= max);
-   return num;
-}
-
-/******************************************************************************
-* STORAGE POINT :: CONSTRUCTOR
-******************************************************************************/
-StoragePoint::StoragePoint(const Position& pt, int value)
-{
-    // initial position is where the bullet was last seen
-    this->pt = pt;
-
-    // positive for a hit, negative for a miss
-    this->value = value;
-
-    // age is initially 100%;
-    age = 1.0;
-
-    // up for positive, down for negative
-    double multiplyFactor = (value <= 0 ? -1.0 : 1.0);
-    v.setDy(randomFloatStorage(1.0, 2.0) * multiplyFactor);
-
-    // move points towards the center of the screen
-    multiplyFactor = (pt.getX() > 400 ? -1.0 : 1.0);
-    v.setDx(randomFloatStorage(1.0, 2.0) * multiplyFactor);
-}
-
-/******************************************************************************
-* STORAGE STANDARD :: CONSTRUCTOR
-******************************************************************************/
-StorageStandard::StorageStandard(double radius, double speed, int points) : StorageBird()
-{
-    // set the position: standard birds start from the middle
-    pt.setY(randomFloatStorage(dimensions.getY() * 0.25, dimensions.getY() * 0.75));
-    pt.setX(0.0);
-
-    // set the velocity
-    v.setDx(randomFloatStorage(speed - 0.5, speed + 0.5));
-    v.setDy(randomFloatStorage(-speed / 5.0, speed / 5.0));
-
-    // set the points
-    this->points = points;
-
-    // set the size
-    this->radius = radius;
-}
-
-/******************************************************************************
-* STORAGE FLOATER :: CONSTRUCTOR
-******************************************************************************/
-StorageFloater::StorageFloater(double radius, double speed, int points) : StorageBird()
-{
-    // floaters start on the lower part of the screen because they go up with time
-    pt.setY(randomFloatStorage(dimensions.getY() * 0.01, dimensions.getY() * 0.5));
-    pt.setX(0.0);
-
-    // set the velocity
-    v.setDx(randomFloatStorage(speed - 0.5, speed + 0.5));
-    v.setDy(randomFloatStorage(0.0, speed / 3.0));
-
-    // set the points value
-    this->points = points;
-
-    // set the size
-    this->radius = radius;
-}
-
-/******************************************************************************
-* STORAGE CRAZY :: CONSTRUCTOR
-******************************************************************************/
-StorageCrazy::StorageCrazy(double radius, double speed, int points) : StorageBird()
-{
-    // crazy birds start in the middle and can go any which way
-    pt.setY(randomFloatStorage(dimensions.getY() * 0.25, dimensions.getY() * 0.75));
-    pt.setX(0.0);
-
-    // set the velocity
-    v.setDx(randomFloatStorage(speed - 0.5, speed + 0.5));
-    v.setDy(randomFloatStorage(-speed / 5.0, speed / 5.0));
-
-    // set the points value
-    this->points = points;
-
-    // set the size
-    this->radius = radius;
-}
-
-/******************************************************************************
-* STORAGE SINKER :: CONSTRUCTOR
-******************************************************************************/
-StorageSinker::StorageSinker(double radius, double speed, int points) : StorageBird()
-{
-    // sinkers start on the upper part of the screen because they go down with time
-    pt.setY(randomFloatStorage(dimensions.getY() * 0.50, dimensions.getY() * 0.95));
-    pt.setX(0.0);
-
-    // set the velocity
-    v.setDx(randomFloatStorage(speed - 0.5, speed + 0.5));
-    v.setDy(randomFloatStorage(-speed / 3.0, 0.0));
-
-    // set the points value
-    this->points = points;
-
-    // set the size
-    this->radius = radius;
-}
-
-/******************************************************************************
-* STORAGE FRAGMENT :: CONSTRUCTOR
-******************************************************************************/
-StorageFragment::StorageFragment(const Position & pt, const Velocity & v) : StorageEffect(pt)
-{
-    // the velocity is a random kick plus the velocity of the thing that died
-    this->v.setDx(v.getDx() * 0.5 + randomFloatStorage(-6.0, 6.0));
-    this->v.setDy(v.getDy() * 0.5 + randomFloatStorage(-6.0, 6.0));
-    
-     // age
-     age = randomFloatStorage(0.4, 1.0);
-     
-     // size
-     size = randomFloatStorage(1.0, 2.5);
-}
-
-/******************************************************************************
-* STORAGE SHRAPNEL :: CONSTRUCTOR
-******************************************************************************/
-StorageShrapnel::StorageShrapnel(StorageBomb& bomb)
-{
-    // how long will this one live?
-    timeToDie = randomIntStorage(5, 15);
-    
-    // The speed and direction is random
-    v.set(randomFloatStorage(0.0, 6.2), randomFloatStorage(10.0, 15.0));
-    pt = bomb.getPosition();
-
-    value = 0;
-    
-    radius = 3.0;
-}
-
-/******************************************************************************
-* STORAGE STREEK :: CONSTRUCTOR
-******************************************************************************/
-StorageStreek::StorageStreek(const Position & pt, Velocity v) : StorageEffect(pt)
-{
-    ptEnd = pt;
-    v *= -1.0;
-    ptEnd += v;
-    
-     // age
-     age = 0.5;
-}
-
-/******************************************************************************
-* STORAGE EXHAUST :: CONSTRUCTOR
-******************************************************************************/
-StorageExhaust::StorageExhaust(const Position & pt, Velocity v) : StorageEffect(pt)
-{
-    ptEnd = pt;
-    v *= -1.0;
-    ptEnd += v;
-
-    // age
-    age = 0.5;
-}
+///******************************************************************
+// * RANDOM
+// * These functions generate a random number.
+// ****************************************************************/
+//int randomIntStorage(int min, int max)
+//{
+//   assert(min < max);
+//   int num = (rand() % (max - min)) + min;
+//   assert(min <= num && num <= max);
+//   return num;
+//}
+//double randomFloatStorage(double min, double max)
+//{
+//   assert(min <= max);
+//   double num = min + ((double)rand() / (double)RAND_MAX * (max - min));
+//   assert(min <= num && num <= max);
+//   return num;
+//}
+//
+///******************************************************************************
+//* STORAGE POINT :: CONSTRUCTOR
+//******************************************************************************/
+//StoragePoint::StoragePoint(const Position& pt, int value)
+//{
+//    // initial position is where the bullet was last seen
+//    this->pt = pt;
+//
+//    // positive for a hit, negative for a miss
+//    this->value = value;
+//
+//    // age is initially 100%;
+//    age = 1.0;
+//
+//    // up for positive, down for negative
+//    double multiplyFactor = (value <= 0 ? -1.0 : 1.0);
+//    v.setDy(randomFloatStorage(1.0, 2.0) * multiplyFactor);
+//
+//    // move points towards the center of the screen
+//    multiplyFactor = (pt.getX() > 400 ? -1.0 : 1.0);
+//    v.setDx(randomFloatStorage(1.0, 2.0) * multiplyFactor);
+//}
+//
+///******************************************************************************
+//* STORAGE STANDARD :: CONSTRUCTOR
+//******************************************************************************/
+//StorageStandard::StorageStandard(double radius, double speed, int points) : StorageBird()
+//{
+//    // set the position: standard birds start from the middle
+//    pt.setY(randomFloatStorage(dimensions.getY() * 0.25, dimensions.getY() * 0.75));
+//    pt.setX(0.0);
+//
+//    // set the velocity
+//    v.setDx(randomFloatStorage(speed - 0.5, speed + 0.5));
+//    v.setDy(randomFloatStorage(-speed / 5.0, speed / 5.0));
+//
+//    // set the points
+//    this->points = points;
+//
+//    // set the size
+//    this->radius = radius;
+//}
+//
+///******************************************************************************
+//* STORAGE FLOATER :: CONSTRUCTOR
+//******************************************************************************/
+//StorageFloater::StorageFloater(double radius, double speed, int points) : StorageBird()
+//{
+//    // floaters start on the lower part of the screen because they go up with time
+//    pt.setY(randomFloatStorage(dimensions.getY() * 0.01, dimensions.getY() * 0.5));
+//    pt.setX(0.0);
+//
+//    // set the velocity
+//    v.setDx(randomFloatStorage(speed - 0.5, speed + 0.5));
+//    v.setDy(randomFloatStorage(0.0, speed / 3.0));
+//
+//    // set the points value
+//    this->points = points;
+//
+//    // set the size
+//    this->radius = radius;
+//}
+//
+///******************************************************************************
+//* STORAGE CRAZY :: CONSTRUCTOR
+//******************************************************************************/
+//StorageCrazy::StorageCrazy(double radius, double speed, int points) : StorageBird()
+//{
+//    // crazy birds start in the middle and can go any which way
+//    pt.setY(randomFloatStorage(dimensions.getY() * 0.25, dimensions.getY() * 0.75));
+//    pt.setX(0.0);
+//
+//    // set the velocity
+//    v.setDx(randomFloatStorage(speed - 0.5, speed + 0.5));
+//    v.setDy(randomFloatStorage(-speed / 5.0, speed / 5.0));
+//
+//    // set the points value
+//    this->points = points;
+//
+//    // set the size
+//    this->radius = radius;
+//}
+//
+///******************************************************************************
+//* STORAGE SINKER :: CONSTRUCTOR
+//******************************************************************************/
+//StorageSinker::StorageSinker(double radius, double speed, int points) : StorageBird()
+//{
+//    // sinkers start on the upper part of the screen because they go down with time
+//    pt.setY(randomFloatStorage(dimensions.getY() * 0.50, dimensions.getY() * 0.95));
+//    pt.setX(0.0);
+//
+//    // set the velocity
+//    v.setDx(randomFloatStorage(speed - 0.5, speed + 0.5));
+//    v.setDy(randomFloatStorage(-speed / 3.0, 0.0));
+//
+//    // set the points value
+//    this->points = points;
+//
+//    // set the size
+//    this->radius = radius;
+//}
+//
+///******************************************************************************
+//* STORAGE FRAGMENT :: CONSTRUCTOR
+//******************************************************************************/
+//StorageFragment::StorageFragment(const Position & pt, const Velocity & v) : StorageEffect(pt)
+//{
+//    // the velocity is a random kick plus the velocity of the thing that died
+//    this->v.setDx(v.getDx() * 0.5 + randomFloatStorage(-6.0, 6.0));
+//    this->v.setDy(v.getDy() * 0.5 + randomFloatStorage(-6.0, 6.0));
+//    
+//     // age
+//     age = randomFloatStorage(0.4, 1.0);
+//     
+//     // size
+//     size = randomFloatStorage(1.0, 2.5);
+//}
+//
+///******************************************************************************
+//* STORAGE SHRAPNEL :: CONSTRUCTOR
+//******************************************************************************/
+//StorageShrapnel::StorageShrapnel(StorageBomb& bomb)
+//{
+//    // how long will this one live?
+//    timeToDie = randomIntStorage(5, 15);
+//    
+//    // The speed and direction is random
+//    v.set(randomFloatStorage(0.0, 6.2), randomFloatStorage(10.0, 15.0));
+//    pt = bomb.getPosition();
+//
+//    value = 0;
+//    
+//    radius = 3.0;
+//}
+//
+///******************************************************************************
+//* STORAGE STREEK :: CONSTRUCTOR
+//******************************************************************************/
+//StorageStreek::StorageStreek(const Position & pt, Velocity v) : StorageEffect(pt)
+//{
+//    ptEnd = pt;
+//    v *= -1.0;
+//    ptEnd += v;
+//    
+//     // age
+//     age = 0.5;
+//}
+//
+///******************************************************************************
+//* STORAGE EXHAUST :: CONSTRUCTOR
+//******************************************************************************/
+//StorageExhaust::StorageExhaust(const Position & pt, Velocity v) : StorageEffect(pt)
+//{
+//    ptEnd = pt;
+//    v *= -1.0;
+//    ptEnd += v;
+//
+//    // age
+//    age = 0.5;
+//}
--- storage_element.h
+++ storage_element.h
@@ -11,6 +11,7 @@ class InterfaceElement;
 ******************************************************************************/
 enum class StorageType
 {
+	NONE,
 	BIRD,
 	BULLET,
 	EFFECT,
@@ -23,42 +24,38 @@ enum class StorageType
 class StorageElement
 {
 protected:
-	/* Note: The Teacher's UML has [pt: Point] However, the class Point 
-	   doesn't exist and the Points class represented the numbers that are 
-	   displayed on the screen when a bird dies (see StoragePoints below). 
-	   The pt should be of type Position, a location. */
     static Position dimensions;
 	Position pt;
 	Velocity v;
 	double radius;
 	int points;
-    int value;
-    double age;
-    double size;
-    int timeToDie;
-    Position ptEnd;
-	bool isDead;
+	int value;
+	StorageType type;
+	double age;
+	double size;
+	int timeToDie;
+	Position ptEnd;
+   	bool dead;
 	LogicElement* pLogic;
 	InterfaceElement* pInterface;
+
 public:
-	// The IteratorBird and IteratorBullet in Storage need this method.
-    StorageElement() : isDead(false) {}
+	StorageElement();
     Position getDimensions() {return dimensions; }
 	Position& getPosition() { return pt; }
 	Velocity& getVelocity() { return v; }
 	double& getRadius() { return radius; }
 	int& getPoints() { return points; }
-    int& getValue() { return value; }
-    double& getAge() { return age; }
-    double& getSize() { return size; }
-    int& getTimeToDie() { return timeToDie; }
-    Position& getPtEnd() { return ptEnd; }
-	bool& getIsDead() { return isDead; }
-    void setIsDead(bool value) { isDead = value; }
+	bool& getIsDead() { return dead; }
 	LogicElement* getLogicElementPtr() { return pLogic; }
 	InterfaceElement* getInterfaceElementPtr() { return pInterface; }
-    
-    virtual StorageType getStorageType() = 0;
+	StorageType getStorageType() { return type;}
+	// Used by Fragment, Streak, Exhaust
+	double& getAge() { return age; }
+	// Used by Fragment
+	double& getSize() { return size; }
+	// Used by Exhaust
+	Position getPtEnd() { return ptEnd; }
 };
 
 /******************************************************************************
@@ -67,15 +64,7 @@ public:
 class StorageBird : public StorageElement
 {
 public:
-    StorageBird(int points = 0, double radius = 1.0) : StorageElement()
-    {
-        this->points = points;
-        this->radius = radius;
-    }
-	virtual StorageType getStorageType() final override
-	{ 
-		return StorageType::BIRD; 
-	}
+	StorageBird() { type = StorageType::BIRD; }
 };
 
 /******************************************************************************
@@ -84,25 +73,7 @@ public:
 class StorageBullet : public StorageElement
 {
 public:
-    StorageBullet(double angle = 0.0, double speed = 30.0, double radius = 5.0, int value = 1) : StorageElement() {
-        this->radius = radius;
-        this->value = value;
-        
-        // set the initial position
-        pt.setX(dimensions.getX() - 1.0);
-        pt.setY(1.0);
-        assert(pt.getX() > 100.0);
-
-        // set the initial velocity
-        v.setDx(-speed * cos(angle));
-        v.setDy(speed * sin(angle));
-        assert(v.getDx() <= 0.0);
-        assert(v.getDy() >= 0.0);
-    }
-	virtual StorageType getStorageType() final override
-	{ 
-		return StorageType::BULLET; 
-	}
+	StorageBullet() { type = StorageType::BULLET; }
 };
 
 /******************************************************************************
@@ -111,130 +82,120 @@ public:
 class StorageEffect : public StorageElement
 {
 public:
-    StorageEffect(const Position & pt)
-    {
-        this->pt = pt;
-        this->age = 0.5;
-    }
-	virtual StorageType getStorageType() final override
-	{ 
-		return StorageType::EFFECT; 
-	}
+	StorageEffect() { type = StorageType::EFFECT; }
 };
 
 /******************************************************************************
 * STORAGE POINT
 ******************************************************************************/
-class StoragePoint : public StorageElement
+class StoragePoints : public StorageElement
 {
 public:
-	// Note: StoragePoint doesn't have children so it's constructor needs to 
-	// be implemented here
-    StoragePoint(const Position& pt, int value);
-	virtual StorageType getStorageType() final override 
-	{ 
-		return StorageType::POINTS; 
-	}
+	StoragePoints() { type = StorageType::POINTS; }
 };
+
+
+
+
 
 /******************************************************************************
 * STORAGE STANDARD 
 ******************************************************************************/
-class StorageStandard : public StorageBird
-{
-public:
-	StorageStandard(double radius = 25.0, double speed = 5.0, int points = 10);
-};
+//class StorageStandard : public StorageBird
+//{
+//public:
+//	StorageStandard(double radius = 25.0, double speed = 5.0, int points = 10);
+//};
 
 /******************************************************************************
 * STORAGE FLOATER 
 ******************************************************************************/
-class StorageFloater : public StorageBird
-{
-public:
-	StorageFloater(double radius = 30.0, double speed = 5.0, int points = 15);
-};
+//class StorageFloater : public StorageBird
+//{
+//public:
+//	StorageFloater(double radius = 30.0, double speed = 5.0, int points = 15);
+//};
 
 /******************************************************************************
 * STORAGE CRAZY 
 ******************************************************************************/
-class StorageCrazy : public StorageBird
-{
-public:
-	StorageCrazy(double radius = 30.0, double speed = 4.5, int points = 30);
-};
+//class StorageCrazy : public StorageBird
+//{
+//public:
+//	StorageCrazy(double radius = 30.0, double speed = 4.5, int points = 30);
+//};
 
 /******************************************************************************
 * STORAGE SINKER 
 ******************************************************************************/
-class StorageSinker : public StorageBird
-{
-public:
-	StorageSinker(double radius = 30.0, double speed = 4.5, int points = 20);
-};
+//class StorageSinker : public StorageBird
+//{
+//public:
+//	StorageSinker(double radius = 30.0, double speed = 4.5, int points = 20);
+//};
 
 /******************************************************************************
 * STORAGE PELLET
 ******************************************************************************/
-class StoragePellet : public StorageBullet
-{
-public:
-    StoragePellet(double angle, double speed = 15.0) : StorageBullet(angle, speed, 1.0, 1) {}
-};
+//class StoragePellet : public StorageBullet
+//{
+//public:
+//    StoragePellet(double angle, double speed = 15.0) : StorageBullet(angle, speed, 1.0, 1) {}
+//};
 
 
 /******************************************************************************
 * STORAGE BOMB 
 ******************************************************************************/
-class StorageBomb : public StorageBullet
-{
-public:
-    StorageBomb(double angle, double speed = 10.0) : StorageBullet(angle, speed, 4.0, 4){
-        timeToDie = 60;
-    }
-};
+//class StorageBomb : public StorageBullet
+//{
+//public:
+//    StorageBomb(double angle, double speed = 10.0) : StorageBullet(angle, speed, 4.0, 4){
+//        timeToDie = 60;
+//    }
+//};
 
 /******************************************************************************
 * STORAGE SHRAPNEL 
 ******************************************************************************/
-class StorageShrapnel : public StorageBullet
-{
-public:
-    StorageShrapnel(StorageBomb & bomb);
-};
+// class StorageShrapnel : public StorageBullet
+//{
+//public:
+//    StorageShrapnel(StorageBomb & bomb);
+//};
 
 /******************************************************************************
 * STORAGE MISSILE 
 ******************************************************************************/
-class StorageMissile : public StorageBullet
-{
-public:
-    StorageMissile(double angle, double speed = 10.0) : StorageBullet(angle, speed, 1.0, 3) {}
-};
+//class StorageMissile : public StorageBullet
+//{
+//public:
+//    StorageMissile(double angle, double speed = 10.0) : StorageBullet(angle, speed, 1.0, 3) {}
+//};
 
 /******************************************************************************
 * STORAGE FRAGMENT
 ******************************************************************************/
-class StorageFragment : public StorageEffect
-{
-public:
-    StorageFragment(const Position & pt, const Velocity & v);
-};
+//class StorageFragment : public StorageEffect
+//{
+//public:
+//    StorageFragment(const Position & pt, const Velocity & v);
+//};
 
 /******************************************************************************
 * STORAGE STREEK
 ******************************************************************************/
-class StorageStreek : public StorageEffect
-{
-public:
-    StorageStreek(const Position & pt, Velocity v);
-};
+//class StorageStreek : public StorageEffect
+//{
+//public:
+//    StorageStreek(const Position & pt, Velocity v);
+//};
 
 /******************************************************************************
 * STORAGE EXHAUST
 ******************************************************************************/
-class StorageExhaust : public StorageEffect
-{
-public:
-    StorageExhaust(const Position & pt, Velocity v);
-};
+//class StorageExhaust : public StorageEffect
+//{
+//public:
+//    StorageExhaust(const Position & pt, Velocity v);
+//};
